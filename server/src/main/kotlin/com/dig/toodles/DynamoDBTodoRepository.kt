6d73c3688f799f87430438c9258388501e7773c9
Tora <toranosuke_kakizono@mail.toyota.co.jp>
2025-01-24 09:33:02 +0900

    DynamoDBファイルの追加

---
+++ server/src/main/kotlin/com/dig/toodles/DynamoDBTodoRepository.kt
@@ -0,0 +1,20 @@
+package com.dig.toodles
+
+class DynamoDBTodoRepository : TodoRepository {
+    override fun getAllTodoItem(): List<TodoItem> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTodoItemById(id: String): TodoItem? {
+        TODO("Not yet implemented")
+    }
+
+    override fun post(todo: TodoItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(id: String) {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ server/src/test/kotlin/com/dig/toodles/DynamoDBTodoRepositoryTest.kt
@@ -0,0 +1,3 @@
+package com.dig.toodles
+
+class DynamoDBTodoRepositoryTest
